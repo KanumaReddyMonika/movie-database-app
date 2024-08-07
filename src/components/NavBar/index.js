c85582abb1159b40e7f37cb9fda6be9cc8ae0d99
KanumaReddyMonika <kanumareddymonika@gmail.com>
2024-08-07 06:21:13 +0000

    initial push movie database

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -8,11 +8,8 @@ const NavBar = props => {
   const renderSearchBar = () => (
     <SearchMoviesContext.Consumer>
       {value => {
-        const {
-          onTriggerSearchingQuery,
-          onChangeSearchInput,
-          searchInput,
-        } = value
+        const {onTriggerSearchingQuery, onChangeSearchInput, searchInput} =
+          value
 
         const onChangeHandler = event => onChangeSearchInput(event.target.value)
 
@@ -24,17 +21,17 @@ const NavBar = props => {
         }
 
         return (
-          <div className="d-flex align-items-center">
+          <div className='d-flex align-items-center'>
             <input
-              type="text"
-              className="me-2 search-input"
+              type='text'
+              className='me-2 search-input'
               onChange={onChangeHandler}
               value={searchInput}
-              placeholder="Search"
+              placeholder='Search'
             />
             <button
-              className="btn btn-outline-info"
-              type="button"
+              className='btn btn-outline-info'
+              type='button'
               onClick={onSearchHandler}
             >
               Search
@@ -46,24 +43,24 @@ const NavBar = props => {
   )
 
   return (
-    <nav className="navbar-container d-flex align-items-center p-3">
-      <div className="logo-container">
-        <h1 className="page-logo">movieDB</h1>
+    <nav className='navbar-container d-flex align-items-center p-3'>
+      <div className='logo-container'>
+        <h1 className='page-logo'>movieDB</h1>
       </div>
-      <div className="ms-auto d-flex align-items-center">
-        <ul className="order-1 d-flex align-items-center p-0 mb-0 ms-3 nav-items-list">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
+      <div className='ms-auto d-flex align-items-center'>
+        <ul className='order-1 d-flex align-items-center p-0 mb-0 ms-3 nav-items-list'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>
               Popular
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/top-rated">
+          <li className='nav-item'>
+            <Link className='nav-link' to='/top-rated'>
               Top Rated
             </Link>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/upcoming">
+          <li className='nav-item'>
+            <Link className='nav-link' to='/upcoming'>
               Upcoming
             </Link>
           </li>
